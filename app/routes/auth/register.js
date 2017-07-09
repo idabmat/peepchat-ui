a6a9291bda9ac43824131af368b243961822f69f
Igor de Alcântara Barroso <idabmat@gmail.com>
2017-07-09 14:25:25 +0100

    Add flash messages

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, inject } = Ember;
+
+export default Route.extend({
+  flash_messages: inject.service(),
   actions: {
     doRegister() {
-      this.get('currentModel').save()
-        .then(() => {
-          this.transitionTo('auth.login');
-        })
+      this.get('currentModel').save().then(() => {
+        this.transitionTo('auth.login');
+        this.get('flashMessages').success('Registered! Please sign in now.');
+      }).catch((response) => {
+        const { errors } = response;
+        this.get('flashMessages').danger(errors.mapBy('detail').join(', '));
+      });
     }
   },
   model() {
