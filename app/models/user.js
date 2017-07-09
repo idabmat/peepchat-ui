6327ebad719de1150ec30927dfbfad8b39096494
Igor de Alcântara Barroso <idabmat@gmail.com>
2017-07-09 23:41:46 +0100

    create rooms + Downgraded Ember to 2.13.3

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,10 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
-export default DS.Model.extend({
-  email: DS.attr('string'),
-  password: DS.attr('string'),
-  passwordConfirmation: DS.attr('string')
+export default Model.extend({
+  email: attr('string'),
+  password: attr('string'),
+  passwordConfirmation: attr('string'),
+  rooms: hasMany('room')
 });
